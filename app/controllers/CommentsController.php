5e79fb9865c8be0c789ebc86782aaf926c5e135a
Lee Han Kyeol <lee0hankyeol@gmail.com>
2014-11-23 03:06:16 +0900

    Implemented comments/store

--- app/controllers/CommentsController.php
+++ app/controllers/CommentsController.php
@@ -2,6 +2,20 @@
 
 class CommentsController extends \BaseController {
 
+	public function postStore() {
+		$user_id = Session::has("user_id")? Session::get("user_id"): 1;
+		$relationship_id = Input::get("relationship_id");
+		$description = Input::get("description");
+
+		$comment = new Comment;
+		$comment->user_id = $user_id;
+		$comment->relationship_id = $relationship_id;
+		$comment->description = $description;
+		$comment->save();
+
+		return $this->returnJson(null, true);
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
