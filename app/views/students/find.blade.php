09dced098bdbd0d6971c597da9c185e33421b95a
Lee Han Kyeol <lee0hankyeol@gmail.com>
2014-11-23 09:21:39 +0900

    Implemented students/search, students/show, users/show, users/update

---
+++ app/views/students/find.blade.php
@@ -0,0 +1,67 @@
+@section('content')
+
+<div class="splash-container">
+	<div class="splash">
+		<h1 class="splash-head">Try your karma.</h1>
+		<p class="splash-subhead">
+			Welcome to Hellgate.
+		</p>
+	</div>
+</div>
+
+<div class="content-wrapper">
+	<div class="ribbon l-box-lrg pure-g">
+		<h2 class="content-head is-center">Request to remember.</h2>
+
+		<div class="pure-g">
+			<div class="l-box-lrg pure-u-1 pure-u-md-2-5">
+				<form class="pure-form pure-form-stacked" method="get" action="/students/show">
+					<fieldset>
+
+						<label for="name">Name</label>
+						<input id="name" name="name" type="text" placeholder="Name">
+
+						<label for="sex">Sex</label>
+						<select id="sex" name="sex" class="pure-input-1-2">
+							<option value="m">남자</option>
+							<option value="f">여자</option>
+						</select>
+
+						<label for="enter_year">Enter Year</label>
+						<input id="enter_year" name="enter_year" placeholder="Enter Year">
+
+						<label for="major">Major</label>
+						<select id="major" name="major" class="pure-input-1-2">
+							<option value="무학과">무학과</option>
+							<option value="건설및환경공학과">건설및환경공학과</option>
+							<option value="경영공학과">경영공학과</option>
+							<option value="기계공학전공">기계공학전공</option>
+							<option value="기술경영학과">기술경영학과</option>
+							<option value="물리학과">물리학과</option>
+							<option value="바이오및뇌공학과">바이오및뇌공학과</option>
+							<option value="산업디자인학과">산업디자인학과</option>
+							<option value="산업및시스템공학과">산업및시스템공학과</option>
+							<option value="생명과학과">생명과학과</option>
+							<option value="생명화학공학과">생명화학공학과</option>
+							<option value="수리과학과">수리과학과</option>
+							<option value="신소재공학과">신소재공학과</option>
+							<option value="원자력및양자공학과">원자력및양자공학과</option>
+							<option value="웹사이언스공학전공">웹사이언스공학전공</option>
+							<option value="인문사회과학과">인문사회과학과</option>
+							<option value="전기및전자공학과">전기및전자공학과</option>
+							<option value="전산학과">전산학과</option>
+							<option value="항공우주공학전공">항공우주공학전공</option>
+							<option value="화학과">화학과</option>
+						</select>
+
+						<button type="submit" class="pure-button">Search</button>
+					</fieldset>
+				</form>
+			</div>
+
+			<div class="l-box-lrg pure-u-1 pure-u-md-3-5">
+			</div>
+		</div>
+	</div>
+</div>
+@stop
---
+++ app/views/students/show.blade.php
@@ -0,0 +1,121 @@
+@section('content')
+
+<div class="splash-container">
+	<div class="splash">
+		<h1 class="splash-head">{{{ substr($student->name, 0, 10) }}}</h1>
+		<p class="splash-subhead">
+			Welcome to Hellgate.
+		</p>
+		<!-- <p>
+			<a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
+		</p> -->
+	</div>
+</div>
+
+<div class="content-wrapper">
+	<div class="content">
+		<h2 class="content-head is-center grey">His or her pasts</h2>
+
+		<div class="pure-g">
+			<div class="l-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-4">
+				<h3 class="content-subhead">
+				Currently
+				<?php $isSolo = true; ?>
+					@foreach ($relationships as $relationship)
+						@if ($relationship->status == 2)
+							On relationship
+							<?php $isSolo = false; ?>
+						@elseif ($relationship->status == 3)
+							Married
+							<?php $isSolo = false; ?>
+						@endif
+					@endforeach
+					@if ($isSolo)
+						Solo
+					@endif
+				</h3>
+				<table class="pure-table" style="margin: 0 auto;">
+					<thead>
+						<tr>
+							<th>Female</th>
+							<th>Male</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						@foreach ($relationships as $relationship)
+						<tr>
+							<td>
+								@if ($relationship->student1->sex == 'm')
+									{{{ $relationship->student2->name }}}
+								@else
+									{{{ $relationship->student1->name }}}
+								@endif
+							</td>
+							<td>
+								@if ($relationship->student1->sex == 'm')
+									{{{ $relationship->student1->name }}}
+								@else
+									{{{ $relationship->student2->name }}}
+								@endif
+							</td>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	<div class="ribbon l-box-lrg pure-g">
+		<h2 class="content-head is-center">Request to remember.</h2>
+
+		<div class="pure-g">
+			<div class="l-box-lrg pure-u-1 pure-u-md-2-5">
+				<form class="pure-form pure-form-stacked" method="get" action="/students/show">
+					<fieldset>
+
+						<label for="name">Name</label>
+						<input id="name" name="name" type="text" placeholder="Name">
+
+						<label for="sex">Sex</label>
+						<select id="sex" name="sex" class="pure-input-1-2">
+							<option value="m">남자</option>
+							<option value="f">여자</option>
+						</select>
+
+						<label for="enter_year">Enter Year</label>
+						<input id="enter_year" name="enter_year" placeholder="Enter Year">
+
+						<label for="major">Major</label>
+						<select id="major" name="major" class="pure-input-1-2">
+							<option value="무학과">무학과</option>
+							<option value="건설및환경공학과">건설및환경공학과</option>
+							<option value="경영공학과">경영공학과</option>
+							<option value="기계공학전공">기계공학전공</option>
+							<option value="기술경영학과">기술경영학과</option>
+							<option value="물리학과">물리학과</option>
+							<option value="바이오및뇌공학과">바이오및뇌공학과</option>
+							<option value="산업디자인학과">산업디자인학과</option>
+							<option value="산업및시스템공학과">산업및시스템공학과</option>
+							<option value="생명과학과">생명과학과</option>
+							<option value="생명화학공학과">생명화학공학과</option>
+							<option value="수리과학과">수리과학과</option>
+							<option value="신소재공학과">신소재공학과</option>
+							<option value="원자력및양자공학과">원자력및양자공학과</option>
+							<option value="웹사이언스공학전공">웹사이언스공학전공</option>
+							<option value="인문사회과학과">인문사회과학과</option>
+							<option value="전기및전자공학과">전기및전자공학과</option>
+							<option value="전산학과">전산학과</option>
+							<option value="항공우주공학전공">항공우주공학전공</option>
+							<option value="화학과">화학과</option>
+						</select>
+
+						<button type="submit" class="pure-button">Search</button>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+@stop
